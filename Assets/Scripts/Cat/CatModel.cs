ceb7fe0078d1c33ffc46948209532128cfcc9cc7
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-07-15 08:28:03 +0530

    Trap the Cat
    Bug Fixes

--- Assets/Scripts/Cat/CatModel.cs
+++ Assets/Scripts/Cat/CatModel.cs
@@ -3,10 +3,5 @@
 public class CatModel
 {
     public Vector2Int CurrentPosition { get; private set; }
-    public float CatSpeed { get; private set; }
-    public CatModel()
-    {
-        CatSpeed = 5f;
-    }
     public void SetCurrentPosition(Vector2Int currentPosition) => CurrentPosition = currentPosition;
 }
